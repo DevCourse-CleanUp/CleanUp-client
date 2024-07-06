bc98c1c7d454f1d5b47f7ad9c35d5334b6ca3fdc
ssoferret <owosso1212@gmail.com>
2024-07-06 22:47:43 +0900

    feat: 글로벌 스타일 컨텍스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import CleanUpThemeProvider from "./context/themeContext";
+import TestContext from "./context/TestContext";
 
 function App() {
-  return <div className="App"> hi</div>;
+  return <div className="App">
+    <CleanUpThemeProvider>
+      <TestContext /> // 컨텍스트 테스트용
+    </CleanUpThemeProvider>
+  </div>;
 }
 
 export default App;
