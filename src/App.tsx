6a5fc87798cb81d229f1e252472134f18bb3a9f3
yoonsaeng <truestar1997@gmail.com>
2024-07-08 15:23:18 +0900

    Refactor: Layout 추가, router 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import CleanUpThemeProvider from "./context/themeContext";
-import TestContext from "./context/TestContext";
+import { CleanUpThemeProvider } from "./context/themeContext";
+import { Layout } from "./components/layout/Layout";
+import { Home } from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
+    // errorElement: <Error />,
+  },
+]);
 
 function App() {
-  return <div className="App">
-    <CleanUpThemeProvider>
-      <TestContext />  {/* 컨텍스트 테스트용 */}
-    </CleanUpThemeProvider>
-  </div>;
+  return (
+    <div className="App">
+      <CleanUpThemeProvider>
+        <RouterProvider router={router} />
+      </CleanUpThemeProvider>
+    </div>
+  );
 }
 
 export default App;
