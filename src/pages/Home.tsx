4fb7d99530479d25e3624222558b040e5290bde7
ssoferret <owosso1212@gmail.com>
2024-07-09 12:40:46 +0900

    Feat: 로그인 상태가 아닐 때 홈에서 로그인 페이지가 나오도록 작성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Login from './Login'
+import useLoginStore from '../store/loginStore'
 
 export const Home = () => {
+  const {isLoggedIn} = useLoginStore();
   return (
-    <div>Home</div>
+    <>
+      {!isLoggedIn && (<Login />)}
+    </>
   )
 }
