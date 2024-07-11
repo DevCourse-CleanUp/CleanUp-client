95c100e46841fab30a19f562ff0be08bbbcd1933
yoonsaeng <truestar1997@gmail.com>
2024-07-11 12:55:17 +0900

    Refactor: 로그인 유무에 따라 보여지는 페이지 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import Login from './Login'
-import { loginState } from '../store/loginAtom'
-import { useRecoilValue } from 'recoil';
+import React from "react";
+import Login from "./Login";
+import { loginState } from "../atoms/loginAtom";
+import { useRecoilState } from "recoil";
+import Lanking from "../components/Lanking/Lanking";
+import Button from "../components/common/Button";
 
-export const Home = () => {
-  const isLoggedIn = useRecoilValue(loginState);
+const Home = () => {
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState(loginState);
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
   return (
     <>
-      {!isLoggedIn && (<Login />)}
+      {!isLoggedIn && <Login />}
+      {isLoggedIn && (
+        <>
+          <Lanking />
+          <Button size="large" scheme="abled" onClick={logoutHandler}>
+            로그아웃
+          </Button>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
+
+export default Home;
