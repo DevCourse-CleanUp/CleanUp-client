cd163b749dce00260850eade5ee5773f21f947eb
Minep <pmh@ge.kornu.ac.kr>
2024-07-15 18:34:05 +0900

    refactor: ProblemZone에서 단계버튼 margin-right추가/ content부분 text-align: justify로 양쪽정렬 추가

--- src/components/Problem/ProblemZone.tsx
+++ src/components/Problem/ProblemZone.tsx
@@ -1,21 +1,21 @@
-import { styled } from 'styled-components';
-import { GoChevronLeft, GoChevronRight } from 'react-icons/go';
-import Button from '../common/Button';
+import { styled } from "styled-components";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
+import Button from "../common/Button";
 
 const problem = {
-    id: 1,
-    title: '쓰레기통',
-    content:
-      '쓰레기들이 쓰레기통 안으로 이동할 수 있도록 도와주세요. 이번에는 아무거나 사용하세요. 이 CSS 속성은 다음의 값들을 인자로 받아 컨테이너 안에서 요소들이 정렬해야 할 방향을 지정합니다. 쓰레기통 안에 쓰레기를 넣으세요. 쓰레기들이 쓰레기통 안으로 이동할 수 있도록 도와주세요. 이번에는 아무거나 사용하세요. 이 CSS 속성은 다음의 값들을 인자로 받아 컨테이너 안에서 요소들이 정렬해야 할 방향을 지정합니다. 쓰레기통 안에 쓰레기를 넣으세요. 쓰레기들이 쓰레기통 안으로 이동할 수 있도록 도와주세요. 이번에는 아무거나 사용하세요. 이 CSS 속성은 다음의 값들을 인자로 받아 컨테이너 안에서 요소들이 정렬해야 할 방향을 지정합니다. 쓰레기통 안에 쓰레기를 넣으세요. ',
-    answer: '쓰레기통',
-    level: 123,
-    score: 9999,
-  };
+  id: 1,
+  title: "쓰레기통",
+  content:
+    "쓰레기들이 쓰레기통 안으로 이동할 수 있도록 도와주세요. 이번에는 아무거나 사용하세요. 이 CSS 속성은 다음의 값들을 인자로 받아 컨테이너 안에서 요소들이 정렬해야 할 방향을 지정합니다. 쓰레기통 안에 쓰레기를 넣으세요. 쓰레기들이 쓰레기통 안으로 이동할 수 있도록 도와주세요. 이번에는 아무거나 사용하세요. 이 CSS 속성은 다음의 값들을 인자로 받아 컨테이너 안에서 요소들이 정렬해야 할 방향을 지정합니다. 쓰레기통 안에 쓰레기를 넣으세요. 쓰레기들이 쓰레기통 안으로 이동할 수 있도록 도와주세요. 이번에는 아무거나 사용하세요. 이 CSS 속성은 다음의 값들을 인자로 받아 컨테이너 안에서 요소들이 정렬해야 할 방향을 지정합니다. 쓰레기통 안에 쓰레기를 넣으세요. ",
+  answer: "쓰레기통",
+  level: 123,
+  score: 9999,
+};
 
 export const ProblemZone = () => {
   return (
     <ProblemZoneStyle>
-        <div className="problemZone">
+      <div className="problemZone">
         <div className="title">
           <h2 className="title">{problem.title}</h2>
           <div className="stageButton">
@@ -33,49 +33,47 @@ export const ProblemZone = () => {
       </div>
     </ProblemZoneStyle>
   );
-}
+};
 
 const ProblemZoneStyle = styled.div`
-  /* .problemZone { */
-    /* background: #ffbebc;
-    padding: 20px; */
+  .stageButton {
+    display: inline-flex;
+    width: auto;
+    background: #5ef0f9;
+    margin-right: 10px;
 
-    .stageButton {
-      display: inline-flex;
-      width: auto;
-      background: #5ef0f9;
-
-      p,
-      svg {
-        display: flex;
-        align-items: center;
-        margin: 0px;
-
-        background: #5ef0f9;
-      }
-
-      p {
-        color: grey;
-      }
-
-      svg {
-        color: #fff;
-      }
-    }
-
-    .title {
+    p,
+    svg {
       display: flex;
-      flex-direction: row;
-      justify-content: space-between;
+      align-items: center;
+      margin: 0px;
 
-      h2 {
-        padding-left: 12px;
-      }
+      background: #5ef0f9;
     }
 
-    .content {
-      padding: 10px;
+    p {
+      color: grey;
     }
+
+    svg {
+      color: #fff;
+    }
+  }
+
+  .title {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    h2 {
+      padding-left: 12px;
+    }
+  }
+
+  .content {
+    text-align: justify;
+    padding: 10px;
+  }
   /* } */
 `;
 
