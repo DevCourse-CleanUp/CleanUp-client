769b3f892ac87bc3b7d9b9a6e8d10d3c48fd636a
ssoferret <owosso1212@gmail.com>
2024-07-18 20:42:37 +0900

    Refact: 오타 수정

--- src/api/http.ts
+++ src/api/http.ts
@@ -20,15 +20,12 @@ export const createClient = (config?: AxiosRequestConfig) => {
 
       // token이 null일 수 있으므로 null 체크를 추가합니다.
       if (token) {
-        let parsedToken: string | null = null;
         try {
-          parsedToken = JSON.parse(token);
+          config.headers.authorization = `Bearer ${token}`;
+          config.headers["Content-Type"] = 'application/json';
+          console.log(config.headers)
         } catch (e) {
           console.error("Failed to parse token:", e);
-        }
-
-        if (parsedToken) {
-          config.headers.authorization = `Bearer ${parsedToken}`;
         }
       }
 
